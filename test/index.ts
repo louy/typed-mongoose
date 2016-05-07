0e81218c59476fc0d95efb5ddd560a4b8d4d807d
Louy <louy08@gmail.com>
2016-05-07 22:24:51 +0100

    add a test

---
+++ test/index.ts
@@ -0,0 +1,9 @@
+import {expect} from 'chai';
+
+import * as mongoose from 'mongoose';
+
+describe('mongoose', () => {
+  it('connects', done => {
+    mongoose.connect('mongodb://127.0.0.1/test', done);
+  });
+});
